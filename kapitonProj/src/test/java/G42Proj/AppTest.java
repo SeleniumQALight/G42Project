15e666a4cfcba0512560ca6822b7d0a6c23e8a65
Taras <radulenko@gmail.com>
2019-11-06 15:37:41 +0200

    13 modules added

---
+++ kapitonProj/src/test/java/G42Proj/AppTest.java
@@ -0,0 +1,20 @@
+package G42Proj;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
