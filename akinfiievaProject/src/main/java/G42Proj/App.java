86a48373968b78ad6babc458eba52e4811cad31f
denis_beztuzhev <epapirua@gmail.com>
2019-11-07 19:22:04 +0200

    test commit 2

--- akinfiievaProject/src/main/java/G42Proj/App.java
+++ akinfiievaProject/src/main/java/G42Proj/App.java
@@ -2,14 +2,12 @@ package G42Proj;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //lol
     }
 
-    //lol
+
 }
