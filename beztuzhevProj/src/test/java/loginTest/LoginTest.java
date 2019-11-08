caaab1d930aabdf8af2bab27eb07823b7b072a6c
dbeztuzhev <epapirua@gmail.com>
2019-11-08 10:47:55 +0200

    11-08 loginTest, pom,drivers commit

---
+++ beztuzhevProj/src/test/java/loginTest/LoginTest.java
@@ -0,0 +1,25 @@
+package loginTest;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+public class LoginTest {
+
+    @Test
+    public void validLogin (){
+        File file = new File("./src/0drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        webDriver.get("http://v3.test.itpmgroup.com/");
+
+
+
+        webDriver.quit();
+    }
+}
