3051e6e41f239b274f5f72f7afc195d272156c50
dbeztuzhev <epapirua@gmail.com>
2019-11-12 21:42:20 +0200

    11-12 commit

---
+++ beztuzhevProj/src/main/java/parentPage/ParentPage.java
@@ -0,0 +1,13 @@
+package parentPage;
+
+import org.openqa.selenium.WebDriver;
+
+public class ParentPage {
+    protected WebDriver webDriver;
+
+    public ParentPage(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+
+}
