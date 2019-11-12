58545388bff92ec4eedb52cf692d962bfa5eeaf4
Hobbit_D <Hobbit.love.vedmochka@gmail.com>
2019-11-12 21:45:15 +0200

    SamoeVajnoe1

---
+++ popelnukhProj/src/test/java/LoginTest/LoginWithPageObject.java
@@ -0,0 +1,11 @@
+package LoginTest;
+
+import abstractParentTest.AbstractParentTest;
+import org.junit.Test;
+
+public class LoginWithPageObject extends AbstractParentTest{
+    @Test
+    public void validLogin(){
+        loginPage.openPage();
+    }
+}
