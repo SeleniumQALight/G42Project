501b0c376fa81b3d7e9122fe12667c779af0666f
VinnyDaPoo <skibaev1990@gmail.com>
2019-11-12 19:38:55 +0200

    Skiba 12.11.19

--- skibaProj/src/test/java/LoginTest/LoginTest.java
+++ skibaProj/src/test/java/LoginTest/LoginTest.java
@@ -13,11 +13,15 @@ public class LoginTest {
     public void validLogin(){
         File file = new File("./src/drivers/chromedriver.exe");
         System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
-        WebDriver webdriver = new ChromeDriver(); //создали объект для браузера хром
+        WebDriver webdriver = new ChromeDriver();                               //создали объект для браузера хром
 
-        webdriver.manage().window().maximize();//сделать окно браузера на весь экран
-        webdriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);//каждые пол секунды по дефолту хром будет пробовать выполнить каждое действие на протяжении 5 секунд, по их истечению будет time timeout
-        webdriver.get("http://v3.test.itpmgroup.com"); //ccылка на наш сайт
-        webdriver.quit();//закрыть браузер
+        webdriver.manage().window().maximize();                                 //сделать окно браузера на весь экран
+        webdriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);      //каждые пол секунды по дефолту хром будет пробовать
+                                                                                //выполнить каждое
+                                                                                //действие на протяжении 5 секунд,
+                                                                                //по их истечению будет time timeout
+
+        webdriver.get("http://v3.test.itpmgroup.com");                          //ccылка на наш сайт
+        webdriver.quit();                                                       //закрыть браузер
     }
 }
