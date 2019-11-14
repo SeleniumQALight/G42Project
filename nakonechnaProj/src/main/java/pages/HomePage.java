e835a30b2be4f74e0f5f834e52ea02aeb9a986eb
mariia13 <tetora94@gmail.com>
2019-11-14 21:29:09 +0200

    seventh

---
+++ nakonechnaProj/src/main/java/pages/HomePage.java
@@ -0,0 +1,10 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import parentPage.ParentPage;
+
+public class HomePage extends ParentPage {
+    public HomePage(WebDriver webDriver) {
+        super(webDriver);
+    }
+}
