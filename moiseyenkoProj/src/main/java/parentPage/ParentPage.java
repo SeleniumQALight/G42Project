328a8e6f400a9b016962cb44c130248d045faf01
LudmilaQA <Ludmila.moiseyenko@gmail.com>
2019-11-14 22:46:11 +0300

    PageObject

--- moiseyenkoProj/src/main/java/parentPage/ParentPage.java
+++ moiseyenkoProj/src/main/java/parentPage/ParentPage.java
@@ -1,11 +1,19 @@
 package parentPage;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
 public class ParentPage {
-   protected WebDriver webDriver;
+    protected WebDriver webDriver;
+    protected Logger logger = Logger.getLogger(getClass());
 
     public ParentPage(WebDriver webDriver) {
         this.webDriver = webDriver;
+    PageFactory.initElements(webDriver,this);
+
+    }
+
+    public void setWebDriver(WebDriver webDriver) {
     }
 }
--- moiseyenkoProj/src/test/java/abstractParentTest/AbstractParentTest.java
+++ moiseyenkoProj/src/test/java/abstractParentTest/AbstractParentTest.java
@@ -1,9 +1,11 @@
 package abstractParentTest;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
 import pages.LoginPage;
 
 import java.io.File;
@@ -12,6 +14,7 @@ import java.util.concurrent.TimeUnit;
 public class AbstractParentTest {
     WebDriver webDriver;
    protected LoginPage loginPage;
+   protected HomePage homePage;
 
     @Before
     public void setUp() {
@@ -23,10 +26,16 @@ public class AbstractParentTest {
         webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
         loginPage = new LoginPage(webDriver);
+        homePage = new HomePage(webDriver);
     }
 
     @After
     public void tearDown() {
         webDriver.quit();
     }
+
+    protected void checkExpectedResult(String massage, boolean actualResult) {
+         Assert.assertEquals(massage,true, actualResult);
+    }
+
 }
