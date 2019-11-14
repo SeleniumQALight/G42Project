6a9c41fa65ecae694369370f1403eabac7de0323
Yerastova <42300597+Yerastova@users.noreply.github.com>
2019-11-14 21:32:03 +0200

    change test framework

--- yerastovaProj/src/test/java/abstractParentTest/AbstractParentTest.java
+++ yerastovaProj/src/test/java/abstractParentTest/AbstractParentTest.java
@@ -1,9 +1,11 @@
 package abstractParentTest;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
 import pages.LoginPage;
 
 import java.io.File;
@@ -12,6 +14,7 @@ import java.util.concurrent.TimeUnit;
 public class AbstractParentTest {
     WebDriver webDriver;
     protected LoginPage loginPage;
+    protected HomePage homePage;
 
     @Before
     public void setUp () {
@@ -22,11 +25,16 @@ public class AbstractParentTest {
         webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
         loginPage = new LoginPage(webDriver);
+        homePage = new HomePage(webDriver);
     }
 
     @After
     public void tearDown() {
         webDriver.quit();
 
+    }
+
+    protected void checkExpectedResult(String message, boolean actualResult) {
+        Assert.assertEquals(message, true, actualResult);
     }
 }
--- yerastovaProj/src/test/java/loginTest/LoginWithPageObjectTest.java
+++ yerastovaProj/src/test/java/loginTest/LoginWithPageObjectTest.java
@@ -8,6 +8,10 @@ public class LoginWithPageObjectTest extends AbstractParentTest {
     @Test
     public void validLogin () {
         loginPage.openPage();
+        loginPage.enterLoginIntoInputLogin("Student");
+        loginPage.enterPassIntoInputPassword("909090");
+        loginPage.clickonButtonVhod();
+        checkExpectedResult("Avatar is not present", homePage.isAvatarDisplayed());
 
     }
 }
