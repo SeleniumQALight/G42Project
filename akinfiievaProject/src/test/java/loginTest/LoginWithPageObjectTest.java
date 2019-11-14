585374d5035fd77311336449dc3ffec4d01dae6a
clementinka <clementine.letto@gmail.com>
2019-11-14 21:29:53 +0200

    14-Nov-19

--- akinfiievaProject/src/test/java/loginTest/LoginWithPageObjectTest.java
+++ akinfiievaProject/src/test/java/loginTest/LoginWithPageObjectTest.java
@@ -7,7 +7,15 @@ import pages.LoginPage;
 public class LoginWithPageObjectTest extends AbstractParentTest {
 
     @Test
-    public void validLogin () {
+    public void validLogin() {
         loginPage.openPage();
+        loginPage.enterLoginIntoInputLogin("Student");
+        loginPage.enterPasswordIntoInputPassword("909090");
+        loginPage.clickOnButtonVhod();
+
+        checkExpectedResult("Avatar is not present", homePage.isAvatarDisplayed());
+
     }
+
+
 }
