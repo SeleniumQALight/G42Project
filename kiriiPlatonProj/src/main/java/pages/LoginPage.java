2dfd600d39c91d974af7eab6c6c7ada8d721b6d6
Platon.Kirii <pkiriy@callfire.com>
2019-11-14 21:30:30 +0200

    Update structure

--- kiriiPlatonProj/src/main/java/pages/LoginPage.java
+++ kiriiPlatonProj/src/main/java/pages/LoginPage.java
@@ -1,10 +1,21 @@
 package pages;
 
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import parentPage.ParentPage;
 
 public class LoginPage extends ParentPage {
+    @FindBy(name = "_username")
+    private WebElement inputLogin;
+
+    @FindBy(id = "password")
+    private WebElement inputPassword;
+
+    @FindBy(tagName = "button")
+    private WebElement vhodButton;
 
     public LoginPage(WebDriver webDriver) {
         super(webDriver);
@@ -18,4 +29,24 @@ public class LoginPage extends ParentPage {
         }
     }
 
+    public void enterLoginIntoInputLogin(String login) {
+//        WebElement inputLogin = webDriver.findElement(By.name("_username"));
+        inputLogin.clear();
+        inputLogin.sendKeys(login);
+        logger.info(login + " was inputed into input Login");
+    }
+
+    public void enterPasswordIntoInputPassword(String password) {
+//        WebElement inputPassword = webDriver.findElement(By.id("password"));
+        inputPassword.clear();
+        inputPassword.sendKeys(password);
+        logger.info(password + " was inputed into input Password");
+    }
+
+
+    public void clickOnButtonVhod() {
+//        WebElement vhodButton = webDriver.findElement(By.tagName("button"));
+        vhodButton.click();
+        logger.info(" Button Vhod was clicked");
+    }
 }
