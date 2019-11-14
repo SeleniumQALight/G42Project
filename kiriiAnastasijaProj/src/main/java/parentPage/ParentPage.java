daf515333ff55e6897d60b0bbea21e2be26fde80
annastaki <annasta@alterpost.org>
2019-11-14 19:07:50 +0200

    14-11-2019

---
+++ kiriiAnastasijaProj/src/main/java/parentPage/ParentPage.java
@@ -0,0 +1,12 @@
+package parentPage;
+
+import org.openqa.selenium.WebDriver;
+
+public class ParentPage {
+    protected WebDriver webDriver;
+    public ParentPage(WebDriver webDriver) {
+
+        this.webDriver = webDriver;
+    }
+
+}
