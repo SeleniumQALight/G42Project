5abad55282ee03b0b45fa5932cce35cf305cf625
qalight <terraco_kiev@ukr.net>
2019-11-14 21:37:11 +0200

    PageObject

--- overAll/src/main/java/pages/LoginPage.java
+++ overAll/src/main/java/pages/LoginPage.java
@@ -2,9 +2,20 @@ package pages;
 
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import parentPage.ParentPage;
 
 public class LoginPage extends ParentPage{
+    @FindBy(name = "_username")
+    private WebElement inputLogin;
+
+    @FindBy(id = "password")
+    private WebElement inputPass;
+
+    @FindBy(tagName = "button")
+    private WebElement buttonVhod;
+
     public LoginPage(WebDriver webDriver) {
         super(webDriver);
     }
@@ -14,7 +25,28 @@ public class LoginPage extends ParentPage{
         try{
             webDriver.get("http://v3.test.itpmgroup.com/");
         }catch (Exception e){
-            Assert.fail("Can not work with browser   hgj");
+            Assert.fail("Can not work with browser");
         }
+    }
+
+    public void enterLoginInToInputLogin(String login) {
+//        WebElement inputLogin = webDriver.findElement(By.name("_username"));
+        inputLogin.clear();
+        inputLogin.sendKeys(login);
+        logger.info(login + " was inputed in to input Login");
+    }
+
+
+    public void enterPassInToInputPassWord(String password) {
+//        WebElement inputPass = webDriver.findElement(By.id("password"));
+        inputPass.clear();
+        inputPass.sendKeys(password);
+        logger.info(password + " was inputed into input PassWord");
+    }
+
+    public void clickOnButtonVhod() {
+//        WebElement buttonVhod = webDriver.findElement(By.tagName("button"));
+        buttonVhod.click();
+        logger.info("Button was clicked");
     }
 }
