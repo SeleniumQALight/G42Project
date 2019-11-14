560288ef6a52b44e700bbe4f76adfc53c8a79e42
Hobbit_D <Hobbit.love.vedmochka@gmail.com>
2019-11-14 21:35:37 +0200

    VeryImportantCommit!

--- popelnukhProj/src/main/java/pages/LoginPage.java
+++ popelnukhProj/src/main/java/pages/LoginPage.java
@@ -1,13 +1,22 @@
 package pages;
 
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import parentPage.ParentPage;
 
 public class LoginPage extends ParentPage {
     public LoginPage(WebDriver webDriver) {
         super(webDriver);
     }
+    @FindBy(name = "_username")
+    private WebElement inputLogin;
+    @FindBy(id = "password")
+    private WebElement inputPassword;
+    @FindBy(tagName = "button")
+    private WebElement buttonVhod;
 
     public void openPage() {
         try{
@@ -15,5 +24,28 @@ public class LoginPage extends ParentPage {
         }catch (Exception e){
             Assert.fail("Can't work with browser");
         }
+    }
+
+    public void enterLoginInToInputLogin(String login) {
+//        WebElement inputLogin = webDriver.findElement(By.name("_username"));
+        inputLogin.clear();
+        inputLogin.sendKeys(login);
+        logger.info(login + " was inputed into input Login");
+
+
+
+    }
+
+    public void enterPassInToInputPassword(String password) {
+//        WebElement inputPassword = webDriver.findElement(By.id("password"));
+        inputPassword.clear();
+        inputPassword.sendKeys(password);
+        logger.info(password + " was inputed into input Password");
+    }
+
+    public void clickOnButtonVhod() {
+        WebElement clickOnButtonVhod = webDriver.findElement(By.tagName("button"));
+        clickOnButtonVhod.click();
+        logger.info("Click was success");
     }
 }
