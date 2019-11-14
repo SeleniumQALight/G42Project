2495750c9907625da779fc7dc0a9ad44649e71f6
mariia13 <tetora94@gmail.com>
2019-11-14 16:07:39 +0200

    fifth commit

--- nakonechnaProj/src/test/java/loginTest/loginTest.java
+++ nakonechnaProj/src/test/java/loginTest/loginTest.java
@@ -1,4 +1,25 @@
 package loginTest;
 
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
 public class loginTest {
+
+    @Test
+    public void validLogin() {
+
+        File file = new File("./src/drivers/chromedriver");
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+        WebDriver webDriver = new ChromeDriver();
+
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        webDriver.get("http://v3.test.itpmgroup.com");
+
+        webDriver.quit();
+    }
 }
