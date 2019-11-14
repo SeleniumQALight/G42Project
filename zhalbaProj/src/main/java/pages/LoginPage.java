8e81f091b9450b1b834efdb82ed4b1c909345af9
azhalba <37302903+azhalba@users.noreply.github.com>
2019-11-14 12:17:53 +0200

    Added login page test (#102)

---
+++ zhalbaProj/src/main/java/pages/LoginPage.java
@@ -0,0 +1,20 @@
+package pages;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import parentPage.ParentPage;
+
+public class LoginPage extends ParentPage {
+
+  public LoginPage(WebDriver webDriver) {
+    super(webDriver);
+  }
+
+  public void openPage() {
+    try {
+      webDriver.get("http://v3.test.itpmgroup.com/");
+    } catch (Exception e) {
+      Assert.fail("Can't work with browser");
+    }
+  }
+}
