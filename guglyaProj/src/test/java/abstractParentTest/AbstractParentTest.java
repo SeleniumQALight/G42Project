e47db5ba38ba825e835fe1ab726b1d44a511e54a
Andrey <andreyguglia@gmail.com>
2019-11-16 17:59:54 +0200

    guglya-12-11

--- guglyaProj/src/test/java/abstractParentTest/AbstractParentTest.java
+++ guglyaProj/src/test/java/abstractParentTest/AbstractParentTest.java
@@ -1,9 +1,11 @@
 package abstractParentTest;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
 import pages.LoginPage;
 
 import java.io.File;
@@ -15,6 +17,7 @@ import java.util.concurrent.TimeUnit;
 public class AbstractParentTest {
     WebDriver webDriver;
     protected LoginPage loginPage;
+    protected HomePage homePage;
 
     @Before
     public void setUp(){
@@ -26,9 +29,13 @@ public class AbstractParentTest {
         webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
         loginPage = new LoginPage(webDriver);
+        homePage = new HomePage((webDriver));
     }
     @After
     public void tearDown(){
         webDriver.quit();
+    }
+    protected void checkExpextedResult(String message, boolean actualResult) {
+        Assert.assertEquals(message, true,actualResult);
     }
 }
