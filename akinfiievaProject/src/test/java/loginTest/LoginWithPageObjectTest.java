cec70880ea3c6d3a5663faf5b71b1dba955e1d4f
clementinka <clementine.letto@gmail.com>
2019-11-17 13:27:48 +0200

    added logging in with not valid login

--- akinfiievaProject/src/test/java/loginTest/LoginWithPageObjectTest.java
+++ akinfiievaProject/src/test/java/loginTest/LoginWithPageObjectTest.java
@@ -18,13 +18,43 @@ public class LoginWithPageObjectTest extends AbstractParentTest {
     }
 
     @Test
-    public void notValidLogin() {
+    public void wrongPassword() {
         loginPage.openPage();
         loginPage.enterLoginIntoInputLogin("Student");
-        loginPage.enterPasswordIntoInputPassword("905090");
+        loginPage.enterPasswordIntoInputPassword("909090909090909099999");
         loginPage.clickOnButtonVhod();
 
         checkExpectedResult("Login page hasn't loaded", homePage.isLoginBoxRefreshed());
     }
 
+    @Test
+    public void wrongLogin() {
+        loginPage.openPage();
+        loginPage.enterLoginIntoInputLogin("Studeeent");
+        loginPage.enterPasswordIntoInputPassword("909090");
+        loginPage.clickOnButtonVhod();
+
+        checkExpectedResult("Login page hasn't loaded", homePage.isLoginBoxRefreshed());
+    }
+
+
+    @Test
+    public void lowcaseLogin() {
+        loginPage.openPage();
+        loginPage.enterLoginIntoInputLogin("student");
+        loginPage.enterPasswordIntoInputPassword("909090");
+        loginPage.clickOnButtonVhod();
+
+        checkExpectedResult("Avatar is not present", homePage.isAvatarDisplayed());
+    }
+
+    @Test
+    public void uppercaseLogin() {
+        loginPage.openPage();
+        loginPage.enterLoginIntoInputLogin("STUDENT");
+        loginPage.enterPasswordIntoInputPassword("909090");
+        loginPage.clickOnButtonVhod();
+
+        checkExpectedResult("Avatar is not present", homePage.isAvatarDisplayed());
+    }
 }
