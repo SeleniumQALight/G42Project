886b8f09156b2b3bd52ceb9058a5b15151552ee5
Andrey Guglia <andreyguglia@gmail.com>
2019-11-18 21:30:28 +0200

    testIn

---
+++ guglyaProj/src/test/java/LoginTest/LoginWithPageObjectTest.java
@@ -0,0 +1,30 @@
+package LoginTest;
+
+import abstractParentTest.AbstractParentTest;
+import org.junit.Test;
+
+public class LoginWithPageObjectTest extends AbstractParentTest {
+    @Test
+    public void validLogin(){
+       loginPage.openPage();
+       loginPage.enterLoginIToInputLogin("Student");
+       loginPage.enterPassInToPassword("909090");
+       loginPage.clickOnButtonVhod();
+
+       checkExpectedResult("Avatar is not present", homePage. isAvatarDisplayed());
+
+
+    }
+
+    @Test
+    public void WrongPass(){
+    loginPage.openPage();
+    loginPage.enterLoginIToInputLogin("Student");
+    loginPage.enterPassInToPassword("906090");
+    loginPage.clickOnButtonVhod();
+
+       checkExpectedResult("WRONG PASSWORD", loginPage.unValidPassW());
+    }
+
+
+}
