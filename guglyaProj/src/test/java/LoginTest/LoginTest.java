092a27675bceb02f3ca66d41d1fdb804d08cfa46
Andrey Guglia <andreyguglia@gmail.com>
2019-11-18 21:30:22 +0200

    testIn

--- guglyaProj/src/test/java/LoginTest/LoginTest.java
+++ guglyaProj/src/test/java/LoginTest/LoginTest.java
@@ -1,7 +1,10 @@
 package LoginTest;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
@@ -11,11 +14,11 @@ import java.util.concurrent.TimeUnit;
  * Created by Андрей Гугля on 07.11.2019.
  */
 public class LoginTest {
-  //  WebDriver webDriver = new ChromeDriver();
+    WebDriver webDriver;
 
     @Test
     public void validLogin(){
-        File file = new File("./src/old_drivers/chromedriver.exe");
+        File file = new File("./src/drivers/chromedriver.exe");
         System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
         WebDriver webDriver = new ChromeDriver();
 
@@ -23,8 +26,26 @@ public class LoginTest {
         webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         webDriver.get("http://v3.test.itpmgroup.com/login");
 
+        webDriver.findElement(By.name("_username")).clear();
+        webDriver.findElement(By.name("_username")).sendKeys("Student");
+        webDriver.findElement(By.id("password")).clear();
+        webDriver.findElement(By.id("password")).sendKeys("909090");
+
+        webDriver.findElement(By.tagName("button")).click();
+
+        Assert.assertTrue("Avatar is not present", isAvatarPresent());
+
 
         webDriver.quit();
 
+    }
+
+    private boolean isAvatarPresent() {
+        try{
+            return webDriver.findElement(By.xpath(".//*[@class='pull-left image']")).isDisplayed();
+        }catch (Exception e){
+            return false;
+
+        }
     }
 }
