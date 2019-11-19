69203523fad28fab7dc07b9ba63173b2e0b19891
annastaki <annasta@alterpost.org>
2019-11-19 16:07:27 +0200

    Invalid test (check Current URL))

--- kiriiAnastasijaProj/src/test/java/loginTest/LoginWithPageObjectTest.java
+++ kiriiAnastasijaProj/src/test/java/loginTest/LoginWithPageObjectTest.java
@@ -23,6 +23,6 @@ public class LoginWithPageObjectTest extends AbstractParentTest {
         loginPage.enterPassInToInputPass("909090");
         loginPage.clickOnButtonVhod();
         Assert.assertEquals(                "Wrong current Page", "http://v3.test.itpmgroup.com/login",
-                loginPage.currentUrl());
+                loginPage.currentPage());
     }
 }
