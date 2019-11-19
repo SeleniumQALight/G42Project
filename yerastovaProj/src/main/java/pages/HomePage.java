3454ad4e145cb570dbea134446609d6a41d64f55
Yerastova <42300597+Yerastova@users.noreply.github.com>
2019-11-19 21:36:15 +0200

    add test

--- yerastovaProj/src/main/java/pages/HomePage.java
+++ yerastovaProj/src/main/java/pages/HomePage.java
@@ -1,24 +1,42 @@
 package pages;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import parentPage.ParentPage;
 
 public class HomePage extends ParentPage {
+    @FindBy (xpath = ".//*[@class='pull-left image']")
+    private WebElement avatar;
+
+    @FindBy (xpath = "//a//span[text()= 'Словари']")
+    private WebElement menuDictionary;
+
+    @FindBy (xpath= "//a[@href='/dictionary/spares']")
+    private WebElement subMenuSpares;
 
     public HomePage(WebDriver webDriver) {
         super(webDriver);
     }
 
     public boolean isAvatarDisplayed () {
-        try {
-            WebElement avatar = webDriver.findElement(By.xpath(".//*[@class='pull-left image']"));
-            return avatar.isDisplayed();
+        return actionsWithOurElements.isElementDisplayed(avatar);
 
-        } catch (Exception e) {
-            return false;
-        }
 
+    }
+
+    public void checkIsAvatarPresent() {
+        Assert.assertTrue("Avatar is not displayed", isAvatarDisplayed());
+    }
+
+    public void clickOnMenuDictionary() {
+        actionsWithOurElements.clickOnElement(menuDictionary);
+    }
+
+    public void clickOnSubMenuSpares() {
+        actionsWithOurElements.clickOnElement(subMenuSpares);
     }
 }
