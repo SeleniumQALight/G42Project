85b462e85f8d3794d511ba43ac18727ced64093c
clementinka <clementine.letto@gmail.com>
2019-11-19 21:33:16 +0200

    19-Nov-19

--- akinfiievaProject/src/main/java/pages/HomePage.java
+++ akinfiievaProject/src/main/java/pages/HomePage.java
@@ -1,32 +1,37 @@
 package pages;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import parentPage.ParentPage;
 
 public class HomePage extends ParentPage {
+    @FindBy(xpath = ".//*[@class='pull-left image']")
+    private WebElement avatar;
+    @FindBy(xpath = ".//li[@id='dictionary']")
+    private WebElement menuDictionary;
+    @FindBy (xpath = ".//li[@id='dictionary']//ul[@class='treeview-menu menu-open']//li[@id='spares']")
+    private WebElement submenuSpares;
+
     public HomePage(WebDriver webDriver) {
         super(webDriver);
     }
 
     public boolean isAvatarDisplayed() {
-        try {
-            WebElement avatar = webDriver.findElement(By.xpath(".//*[@class='pull-left image']"));
-        } catch (Exception e) {
-            return false;
-        }
-        return true;
+       return actionsWithOurElements.isElementDisplayed(avatar);
     }
 
-    public boolean isLoginBoxRefreshed() {
+    public void checkIsAvatarPresent() {
+        Assert.assertTrue("Avatar is not displayed", isAvatarDisplayed());
+    }
 
-        try {
-            WebElement loginLogo = webDriver.findElement(By.xpath(".//div[@class='login-box-body']"));
+    public void clickOnMenuDictionary() {
+        actionsWithOurElements.clickOnElement(menuDictionary);
+    }
 
-        } catch (Exception e) {
-            return false;
-        }
-        return true;
+    public void clickOnSubmenuSpares() {
+        actionsWithOurElements.clickOnElement(submenuSpares);
     }
 }
