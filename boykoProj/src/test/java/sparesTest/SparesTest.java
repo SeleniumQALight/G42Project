c8e44bef7559d42b5bc13eb89b5b812bfa672f83
janne-boyko <zhannaboyko1990@gmail.com>
2019-11-21 21:38:34 +0200

    21-11-19 homework

--- boykoProj/src/test/java/sparesTest/SparesTest.java
+++ boykoProj/src/test/java/sparesTest/SparesTest.java
@@ -1,9 +1,11 @@
 package sparesTest;
 
 import abstractParentTest.AbstractParentTest;
+import org.junit.After;
 import org.junit.Test;
 
 public class SparesTest extends AbstractParentTest {
+    private final String spareName = "BoykoZhannaSpare";
     @Test
     public void addNewSpare(){
         loginPage.fillingLoginFormAndSubmitIt("Student", "909090");
@@ -11,10 +13,20 @@ public class SparesTest extends AbstractParentTest {
         homePage.clickOnMenuDictionary();
         homePage.clickOnSubmenuSpares();
         sparePage.clickOnAddButton();
-        editSparePage.enterSpareName("BoykoZhannaSpare1");
-        editSparePage.selectSpareTypeFromDropDown(); //датчики
+//        editSparePage.enterSpareName("BoykoZhannaSpare1");
+//        editSparePage.selectSpareTypeFromDropDown(); //датчики
+        editSparePage.enterSpareNameInToInput(spareName);
+        editSparePage.selectSpareTypeFromDropDown("Датчики");
         editSparePage.clickOnSubmitButton();
-        checkExpectedResult("Spare is not found", sparePage.isSpareDisplayed());
+//        checkExpectedResult("Spare is not found", sparePage.isSpareDisplayed());
+        checkExpectedResult("Can not find spare in List", sparePage.isSpareInList(spareName));
+
+
+    }
+
+    @After
+    public void deleteSpare (){
+        sparePage.deleteSpareUntilPresent(spareName);
 
     }
 }
