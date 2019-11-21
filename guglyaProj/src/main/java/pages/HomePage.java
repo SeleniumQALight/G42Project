45ddce5b127c24fb56d7cf02fab7bf3695d9a367
Taras21 <automation@qalight.com.ua>
2019-11-21 16:49:29 +0200

    guglya-fix

--- guglyaProj/src/main/java/pages/HomePage.java
+++ guglyaProj/src/main/java/pages/HomePage.java
@@ -10,27 +10,27 @@ import parentPage.ParentPage;
 /**
  * Created by Андрей Гугля on 14.11.2019.
  */
-public class HomePage extends ParentPage{
-    @FindBy (xpath = ".//*[@class='pull-left image']")
-    private  WebElement avatar;
+public class HomePage extends ParentPage {
+    @FindBy(xpath = ".//*[@class='pull-left image']")
+    private WebElement avatar;
 
-    @FindBy (id = "dictionary")
+    @FindBy(id = "dictionary")
     private WebElement menuDictionary;
 
-    @FindBy (id = "spares")
+    @FindBy(id = "spares")
     private WebElement subMenuSpares;
 
     public HomePage(WebDriver webDriver) {
         super(webDriver);
     }
-    public boolean isAvatarDisplayed(){
-     //   try {
-       //     WebElement avatar = webDriver.findElement(By.xpath(""));
-        //    return isAvatarDisplayed();
-     //   }catch (Exception e) {
-          //  return false;
-        //}
 
+    public boolean isAvatarDisplayed() {
+        //   try {
+        //     WebElement avatar = webDriver.findElement(By.xpath(""));
+        //    return isAvatarDisplayed();
+        //   }catch (Exception e) {
+        //  return false;
+        //}
 
 
         return actionsWithOurElements.isElementDisplayed(avatar);
