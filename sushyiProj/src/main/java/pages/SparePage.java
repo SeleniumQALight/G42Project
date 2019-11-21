e8cc83a589e11492b236c549b82546abcf8acb0f
sushy <sushyi_av@ukr.net>
2019-11-21 21:32:54 +0200

    lekcia za 21.11

--- sushyiProj/src/main/java/pages/SparePage.java
+++ sushyiProj/src/main/java/pages/SparePage.java
@@ -9,11 +9,35 @@ public class SparePage extends ParentPage {
     @FindBy(xpath = "//*[@class='fa fa-plus']")
     private WebElement buttonAdd;
 
+    private EditSparePage clickOnSparePage;
+
     public SparePage(WebDriver webDriver) {
         super(webDriver);
     }
 
     public void clickOnAddButton() {
         actionsWithOurElements.clickOnElement(buttonAdd);
+    }
+
+    public boolean isSpareInList(String spareName) {
+        //tyt isp Polimorfizm
+        return actionsWithOurElements
+                .isElementDisplayed(".//*[text()='" + spareName + "']");
+    }
+
+    public void deleteSpareUntilPresent(String spareName) {
+        EditSparePage editSparePage = new EditSparePage(webDriver);
+        //poka ne stanet False
+        while (isSpareInList(spareName)){
+            cllickOnSpare(spareName);
+            editSparePage.clickOnDeleteButton();
+            //чтоб он был не Вечным то сделаем Каунтер
+        }
+
+    }
+
+    private void cllickOnSpare(String spareName) {
+        actionsWithOurElements
+                .clickOnElement(".//*[text()='" + spareName + "']");
     }
 }
--- sushyiProj/src/test/java/sparesTests/SparesTest.java
+++ sushyiProj/src/test/java/sparesTests/SparesTest.java
@@ -1,11 +1,14 @@
 package sparesTests;
 
 import abstractParentTest.AbstractParentTest;
+import org.junit.After;
 import org.junit.Test;
 
 public class SparesTest extends AbstractParentTest{
+    private  final String spareName = "RasulenkoSpare";
     @Test
     public void addNewSpare(){
+
         loginPage.fillingLoginFormAndSubmitIt("Student", "909090");
 
         homePage.checkIsAvatarPresent();
@@ -13,6 +16,17 @@ public class SparesTest extends AbstractParentTest{
         homePage.clickOnSubMenuSpares();
 
         sparePage.clickOnAddButton();
+        editSparePage.enterSpareNameIntoInput(spareName);
+        editSparePage.selectTypeFromDropDown("Датчики");
+        editSparePage.clickOnSubmitButton();
 
+        checkExpectedResult("Can not find spare in list"
+        , sparePage.isSpareInList(spareName));
+
+    }
+
+    @After
+    public void deleteSpare(){
+        sparePage.deleteSpareUntilPresent(spareName);
     }
 }
