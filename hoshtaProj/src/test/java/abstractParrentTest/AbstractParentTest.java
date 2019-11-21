726306b124957263757cbb71715366a1bb843d45
volodymyr.hoshta <vhoshta@callfire.com>
2019-11-21 19:56:53 +0200

    test message

--- hoshtaProj/src/test/java/abstractParrentTest/AbstractParentTest.java
+++ hoshtaProj/src/test/java/abstractParrentTest/AbstractParentTest.java
@@ -17,7 +17,7 @@ public class AbstractParentTest {
     private WebDriver webDriver;
     protected LoginPage loginPage;
     protected HomePage homePage;
-    protected SparesPage sparesPage;
+    protected SparesPage sparesPage;//text
     protected EditSparePage editSparePage;
 
     @Before
