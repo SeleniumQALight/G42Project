59f548187931341e4ca4d7e0b105add0ff47a318
Yerastova <42300597+Yerastova@users.noreply.github.com>
2019-11-24 15:18:42 +0200

    add method for work with DropDown

--- yerastovaProj/src/main/java/pages/SparePage.java
+++ yerastovaProj/src/main/java/pages/SparePage.java
@@ -16,4 +16,20 @@ public class SparePage extends ParentPage {
     public void clickOnAddButton() {
         actionsWithOurElements.clickOnElement(addButton);
     }
+
+    public boolean isSpareInList(String spareName) {
+        return actionsWithOurElements.isElementDisplayed(".//*[text() = '" + spareName + "']");
+    }
+
+    public void deleteSpareUntilPresent(String spareName) {
+        EditSparePage editSparePage = new EditSparePage(webDriver);
+        while (isSpareInList(spareName)){
+            clickOnSpare(spareName);
+            editSparePage.clickOnDeleteButton();
+        }
+    }
+
+    private void clickOnSpare(String spareName) {
+        actionsWithOurElements.clickOnElement(".//*[text() = '" + spareName + "']");
+    }
 }
