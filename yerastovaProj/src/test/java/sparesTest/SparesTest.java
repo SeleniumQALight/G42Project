05affb23fbb58657cf112c588f95398ef2422bd2
Yerastova <42300597+Yerastova@users.noreply.github.com>
2019-11-24 10:32:55 +0200

    add new Page EditSparePage add method into EditSparePage for HW11

--- yerastovaProj/src/test/java/sparesTest/SparesTest.java
+++ yerastovaProj/src/test/java/sparesTest/SparesTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 public class SparesTest extends AbstractParentTest {
     @Test
     public  void addNewSpare () {
+        final String spareName = "RadulenkoSpare";
         loginPage.fillingLoginFormAndSubmitIt("Student", "909090");
 
         homePage.checkIsAvatarPresent();
@@ -13,6 +14,10 @@ public class SparesTest extends AbstractParentTest {
         homePage.clickOnSubMenuSpares();
 
         sparePage.clickOnAddButton();
+
+        editSparePage.enterSpareName(spareName);
+        editSparePage.selectSpareTypeFromDropDown("Датчики");
+
 
 
     }
