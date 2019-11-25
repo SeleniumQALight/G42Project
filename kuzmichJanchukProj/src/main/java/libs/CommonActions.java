4882b18a365aeedc2906e098667879283d3d865b
GunkanjimaQA <47102954+GunkanjimaQA@users.noreply.github.com>
2019-11-25 23:42:17 +0200

    Added classwork of 21.11.2019 Added homework 12 -- toggleCheckbox() method

--- kuzmichJanchukProj/src/main/java/libs/CommonActions.java
+++ kuzmichJanchukProj/src/main/java/libs/CommonActions.java
@@ -2,8 +2,10 @@ package libs;
 
 import org.apache.log4j.Logger;
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class CommonActions {
     WebDriver driver;
@@ -48,4 +50,69 @@ public class CommonActions {
         Assert.fail("Can't work with element ");
     }
 
+    public void selectValueInDDByJava(WebElement dropDown, String value) {
+        try {
+            Select select = new Select(dropDown);
+            select.selectByValue(value);
+            log.info(value + " was selected in Drop Down");
+        } catch (Exception e) {
+            stopTestAndPrintMessage();
+        }
+    }
+
+    public void selectVisibleTextInDDByJava(WebElement dropDown, String text) {
+        try {
+            Select select = new Select(dropDown);
+            select.selectByVisibleText(text);
+            log.info(text + " was selected in Drop Down");
+        } catch (Exception e) {
+            stopTestAndPrintMessage();
+        }
+    }
+
+    public boolean isElementDisplayed(String locator) {
+        try {
+            return isElementDisplayed(driver.findElement(By.xpath(locator)));
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public void clickOnElement(String xpath) {
+        try {
+            clickOnElement(driver.findElement(By.xpath(xpath)));
+        } catch (Exception e) {
+            stopTestAndPrintMessage();
+        }
+    }
+
+    public void toggleCheckbox(WebElement checkboxLocator, String action) {
+        switch (action) {
+            case "check":
+                try {
+                    if (!checkboxLocator.isSelected()) {
+                        checkboxLocator.click();
+                    } else {
+                        log.info("Checkbox is already checked!");
+                    }
+                } catch (Exception e) {
+                    stopTestAndPrintMessage();
+                }
+                break;
+            case "uncheck":
+                try {
+                    if (checkboxLocator.isSelected()) {
+                        checkboxLocator.click();
+                    } else {
+                        log.info("Checkbox is already unchecked!");
+                    }
+                } catch (Exception e) {
+                    stopTestAndPrintMessage();
+                }
+                break;
+            default:
+                log.error("Wrong action parameter! Use <check> or <uncheck> only.");
+                break;
+        }
+    }
 }
