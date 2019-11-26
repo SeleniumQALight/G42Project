f2a0ab2c0200906a5c81df7b0b2086ab37b9a063
Martsekhov <56881119+Martsekhov@users.noreply.github.com>
2019-11-26 21:53:10 +0200

    11-26 Martsekhov Spares URL

--- martsekhovProj/src/test/java/sparesTests/SparesTest.java
+++ martsekhovProj/src/test/java/sparesTests/SparesTest.java
@@ -1,31 +1,38 @@
 package sparesTests;
 
 import abstractParentTest.AbstractParentTest;
+import libs.Utils;
 import org.junit.After;
 import org.junit.Test;
 
 public class SparesTest extends AbstractParentTest {
-    private final String spareName = "MartsekhovSpare";
+    private final String spareName = "MartsekhovSpare " + Utils.getDateAndTimeFormated();
     @Test
     public void addNewSpare(){
         loginPage.fillingLoginFormAndSubmitIt("Student", "909090");
+        homePage.checkCurrentUrl();
         homePage.checkIsAvatarPresent();
         homePage.clickOnMenuDictionary();
         homePage.clickOnSubMenuSpares();
+
+        sparePage.checkCurrentUrl();
+        sparePage.deleteSpareUntilPresent(spareName);
         sparePage.clickOnAddButton();
 
-
+    editSparePage.checkCurrentUrl();
     editSparePage.enterSpareNameInToInput(spareName);
     editSparePage.selectSpareTypeFromDropDown("Датчики");
     editSparePage.clickOnSubmitButton();
 
-    checkExpectedResult ("Can not find spare in list"
-            , sparePage.isSpareInList (spareName));
+    sparePage.checkCurrentUrl();
+
+
+    checkExpectedResult ("Can not find spare in list", sparePage.isSpareInList (spareName));
 
     }
     @After
 
     public void deleteSpare(){
-      sparePage.deleteSpareUntilPresent(spareName);
+        sparePage.deleteSpareUntilPresent(spareName);
     }
 }
