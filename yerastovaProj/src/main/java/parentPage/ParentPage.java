47596174d1af780c912ba52bcb230024e4bed8fb
Yerastova <42300597+Yerastova@users.noreply.github.com>
2019-11-26 21:30:05 +0200

    add properties

--- yerastovaProj/src/main/java/parentPage/ParentPage.java
+++ yerastovaProj/src/main/java/parentPage/ParentPage.java
@@ -1,6 +1,8 @@
 package parentPage;
 
 import libs.ActionsWithOurElements;
+import libs.ConfigProperties;
+import org.aeonbits.owner.ConfigFactory;
 import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
@@ -10,12 +12,15 @@ public class ParentPage {
     protected WebDriver webDriver;
     protected Logger logger = Logger.getLogger(getClass());
     protected ActionsWithOurElements actionsWithOurElements;
-    String baseURL = "http://v3.test.itpmgroup.com";
+
+    public ConfigProperties configProperties = ConfigFactory.create(ConfigProperties.class);
+    String baseURL;
     String expectedURL;
 
     public ParentPage(WebDriver webDriver, String partURL) {
 
         this.webDriver = webDriver;
+        baseURL = configProperties.base_url();
         PageFactory.initElements(webDriver, this);
         actionsWithOurElements = new ActionsWithOurElements(webDriver);
         expectedURL = baseURL + partURL;
