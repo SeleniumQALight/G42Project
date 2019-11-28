cbe654dd1e51a7a6a61eb7e628c759caf7eac4d6
Anton Zhalba <azhalba@callfire.com>
2019-11-28 21:31:23 +0200

    Work in class 28.11

--- zhalbaProj/src/test/java/abstractParentTest/AbstractParentTest.java
+++ zhalbaProj/src/test/java/abstractParentTest/AbstractParentTest.java
@@ -1,12 +1,15 @@
 package abstractParentTest;
 
-import java.io.File;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import java.util.concurrent.TimeUnit;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import pages.EditSparePage;
 import pages.HomePage;
 import pages.LoginPage;
@@ -21,10 +24,11 @@ public class AbstractParentTest {
   protected EditSparePage editSparePage;
 
   @Before
-  public void setUp() {
-    File file = new File("./src/drivers/chromedriver.exe");
-    System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
-    webDriver = new ChromeDriver();
+  public void setUp() throws Exception {
+//    File file = new File("./src/drivers/chromedriver.exe");
+////    System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+////    webDriver = new ChromeDriver();
+    webDriver = driverInit();
     webDriver.manage().window().maximize(); // окно на весь экран
     webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     loginPage = new LoginPage(webDriver);
@@ -32,6 +36,25 @@ public class AbstractParentTest {
     sparePage = new SparePage(webDriver);
     editSparePage = new EditSparePage(webDriver);
 
+  }
+
+  private WebDriver driverInit() throws Exception {
+    String browser = System.getProperty("browser");
+    if ((browser == null) || ("chrome".equals(browser))) {
+      WebDriverManager.chromedriver().setup();
+      return new ChromeDriver();
+    } else if ("firefox".equalsIgnoreCase(browser)) {
+      WebDriverManager.firefoxdriver().setup();
+      return new FirefoxDriver();
+    } else if ("ie".equalsIgnoreCase(browser)) {
+      WebDriverManager.iedriver().arch32().setup();
+      return new InternetExplorerDriver();
+    } else if ("edge".equalsIgnoreCase(browser)) {
+      WebDriverManager.edgedriver().setup();
+      return new EdgeDriver();
+    } else {
+      throw new Exception("Check browser var ");
+    }
   }
 
   @After
