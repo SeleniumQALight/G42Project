641b394e53abcddbe36bf43a458ade570c4a2e2e
Martsekhov <56881119+Martsekhov@users.noreply.github.com>
2019-11-28 21:33:10 +0200

    11-28 Martsekhov Browsers

--- martsekhovProj/src/main/java/libs/ActionWithOurElements.java
+++ martsekhovProj/src/main/java/libs/ActionWithOurElements.java
@@ -4,13 +4,18 @@ import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ActionWithOurElements {
     WebDriver webDriver;
     Logger logger = Logger.getLogger(getClass());
+    WebDriverWait webDriverWait_10, getWebDriverWait_15;
 
     public ActionWithOurElements(WebDriver webDriver) {
         this.webDriver = webDriver;
+        webDriverWait_10 = new WebDriverWait(webDriver, 10);
+        getWebDriverWait_15 = new WebDriverWait(webDriver,15);
     }
     public void enterTextInToInput(WebElement webElement, String text){
         try { webElement.clear();
@@ -23,6 +28,7 @@ public class ActionWithOurElements {
 
     public void clickOnElement(WebElement webElement){
         try{
+            webDriverWait_10.until(ExpectedConditions.elementToBeClickable(webElement));
             webElement.click();
             logger.info("Element was clicked");
         }catch (Exception e){
--- martsekhovProj/src/test/java/abstractParentTest/AbstractParentTest.java
+++ martsekhovProj/src/test/java/abstractParentTest/AbstractParentTest.java
@@ -1,11 +1,14 @@
 package abstractParentTest;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
 
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import pages.HomePage;
 import pages.LoginPage;
 import pages.SparePage;
@@ -21,16 +24,35 @@ public class AbstractParentTest {
 
 
     @Before
-    public void setUp() {
-        File file = new File("./src/drivers/chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
-        webDriver = new ChromeDriver();
+    public void setUp() throws Exception {
+        //File file = new File("./src/drivers/chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+        //webDriver = new ChromeDriver();
+        webDriver = driverInit();
+
+
 
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         loginPage = new LoginPage(webDriver);
         homePage = new HomePage(webDriver);
         sparePage = new SparePage(webDriver);
+    }
+
+    private WebDriver driverInit() throws Exception {
+        String browser = System.getProperty("browser");
+        if((browser == null) || ("chrome".equalsIgnoreCase(browser))){
+            WebDriverManager.chromedriver().setup();
+            return new ChromeDriver();
+        } else  if("firefox".equalsIgnoreCase(browser)){
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+        } else if ("ie".equalsIgnoreCase(browser)){
+            WebDriverManager.iedriver().arch32().setup();
+            return new InternetExplorerDriver();
+        }else {
+            throw new Exception("Check browser var ");
+        }
     }
 
     @After
