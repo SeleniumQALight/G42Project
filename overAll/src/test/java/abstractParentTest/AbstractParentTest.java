a497c2def3a9de607a917804db01905c2d81fa40
Taras21 <automation@qalight.com.ua>
2019-11-28 14:00:27 +0200

    WebDriver manager

--- overAll/src/test/java/abstractParentTest/AbstractParentTest.java
+++ overAll/src/test/java/abstractParentTest/AbstractParentTest.java
@@ -1,10 +1,13 @@
 package abstractParentTest;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import pages.EditSparePage;
 import pages.HomePage;
 import pages.LoginPage;
@@ -14,17 +17,20 @@ import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 public class AbstractParentTest {
-    WebDriver webDriver;
+    private WebDriver webDriver;
     protected LoginPage loginPage;
     protected HomePage homePage;
     protected SparePage sparePage;
     protected EditSparePage editSparePage;
 
     @Before
-    public void setUp(){
-        File file = new File("./src/drivers/chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
-        webDriver = new ChromeDriver();
+    public void setUp() throws Exception {
+//        File file = new File("./src/drivers/chromedriver.exe");
+//        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+//        webDriver = new ChromeDriver();
+//        WebDriverManager.chromedriver().setup();
+//        webDriver = new ChromeDriver();
+        webDriver = driverInit();
 
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
@@ -49,5 +55,23 @@ public class AbstractParentTest {
     protected void checkExpectedResult(String message,boolean expectedResult
             , boolean actualResult) {
         Assert.assertEquals(message, expectedResult, actualResult);
+    }
+
+    private WebDriver driverInit() throws Exception {
+        String browser = System.getProperty("browser");
+        if ((browser == null) || "chrome".equals(browser.toLowerCase())){
+            WebDriverManager.chromedriver().setup();
+            return new ChromeDriver();
+        } else if ("firefox".equals(browser.toLowerCase())){
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+        } else if ("ie".equals(browser.toLowerCase())){
+            // https://github.com/bonigarcia/webdrivermanager
+            WebDriverManager.iedriver().arch32().setup();
+            return new InternetExplorerDriver();
+        } else {
+            throw new Exception("Check browser var");
+        }
+
     }
 }
