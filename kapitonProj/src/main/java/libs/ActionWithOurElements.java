3b22c012bbee5fa55732b3bce924c203962bdccc
KapitonArtem <kapiton2007@ukr.net>
2019-12-01 19:01:33 +0200

    26-11

--- kapitonProj/src/main/java/libs/ActionWithOurElements.java
+++ kapitonProj/src/main/java/libs/ActionWithOurElements.java
@@ -6,15 +6,20 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ActionWithOurElements {
     WebDriver webDriver;
     Logger logger = Logger.getLogger(getClass());
+    WebDriverWait webDriverWait_10 , webDriverWait_15;
 
 
     public ActionWithOurElements(WebDriver webDriver) {
         this.webDriver = webDriver;
+        webDriverWait_10 = new WebDriverWait(webDriver, 10);
+        webDriverWait_15 = new WebDriverWait(webDriver, 15);
     }
 
     public void enterTextInToInput(WebElement webElement, String text) {
@@ -32,6 +37,8 @@ public class ActionWithOurElements {
 
     public void clickOnElement(WebElement webElement) {
         try {
+            webDriverWait_10.until(ExpectedConditions.elementToBeClickable(webElement));
+            //webDriverWait_10.until(ExpectedConditions.not(ExpectedConditions.elementToBeClickable(webElement)));
             webElement.click();
             logger.info("Element was clicked");
         } catch (Exception e) {
