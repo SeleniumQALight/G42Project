ca739fd41afb956c792d0bcc46b9e4948b77d0d2
LudmilaQA <Ludmila.moiseyenko@gmail.com>
2019-12-03 22:37:13 +0300

    Class work HTML Yandex Elements, work with Suits

--- moiseyenkoProj/src/main/java/libs/ActionsWithOurElements.java
+++ moiseyenkoProj/src/main/java/libs/ActionsWithOurElements.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import ru.yandex.qatools.htmlelements.element.TypifiedElement;
 
 public class ActionsWithOurElements {
     WebDriver webDriver;
@@ -41,11 +42,19 @@ public class ActionsWithOurElements {
  //         Это метод для  подождать пока эллемент не исчезнет
 
             webElement.click();
-            logger.info("Element was clicked");
+            logger.info("Element was clicked" + getElementName(webElement));
 
         } catch (Exception e) {
             stopTestAndPrintMessage();
         }
+    }
+
+    private String getElementName(WebElement webElement) {
+        String elementName = "";
+        if (webElement instanceof TypifiedElement) {
+            elementName = "'" + ((TypifiedElement) webElement).getName() + "'";
+        }
+        return elementName;
     }
 
     public boolean isElementDisplayed (WebElement webElement) {
