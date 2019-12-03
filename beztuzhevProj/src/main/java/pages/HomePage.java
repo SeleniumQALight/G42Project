bdd5d6254fcb25ccb9fc9c8ed6f6c0fb2346dc6b
dbeztuzhev <epapirua@gmail.com>
2019-12-03 21:43:50 +0200

    03-12 classWork

--- beztuzhevProj/src/main/java/pages/HomePage.java
+++ beztuzhevProj/src/main/java/pages/HomePage.java
@@ -1,25 +1,31 @@
 package pages;
 
 import org.junit.Assert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import pages.pageElements.LeftMenu;
 import parentPage.ParentPage;
+import ru.yandex.qatools.htmlelements.annotations.Name;
+import ru.yandex.qatools.htmlelements.element.Link;
+
 
 public class HomePage extends ParentPage {
 
     @FindBy(xpath = ".//*[@class='pull-left image']")
     private WebElement avatar;
 
-    @FindBy (id = "dictionary")
-    private WebElement menuDictionary;
+    //@Name("Menu Dictionary")
+    //@FindBy (id = "dictionary")
+    // private Link menuDictionary;
 
-    @FindBy (id = "spares")
-    private WebElement subMenuSpares;
+    // @FindBy (id = "spares")
+    //private Link subMenuSpares;
 
-    @FindBy (id = "prov_cus")
-    private WebElement menuProviders;
+    @FindBy(id = "prov_cus")
+    private Link menuProviders;
+
+    public LeftMenu leftMenu;
 
     public HomePage(WebDriver webDriver) {
         super(webDriver, "/");
@@ -39,15 +45,16 @@ public class HomePage extends ParentPage {
         Assert.assertTrue("Avatar is not displayed", isAvatarDisplayed());
     }
 
-    public void clickOnMenuDictionary() {
-        actionsWithOurElements.clickOnElement(menuDictionary);
-    }
+    //public void clickOnMenuDictionary() {
+    //actionsWithOurElements.clickOnElement(menuDictionary);
+    // }
 
-    public void clickOnSubMenuSpares() {
-        actionsWithOurElements.clickOnElement(subMenuSpares);
-    }
+    //public void clickOnSubMenuSpares() {
+    //actionsWithOurElements.clickOnElement(subMenuSpares);
+    //}
 
-    public void clickOnMenuProviders() { actionsWithOurElements.clickOnElement(menuProviders);
+    public void clickOnMenuProviders() {
+        actionsWithOurElements.clickOnElement(menuProviders);
 
     }
 }
