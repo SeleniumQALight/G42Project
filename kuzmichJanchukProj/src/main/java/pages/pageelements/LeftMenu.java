94b314b7971f086fe8e2b57f98a2f185479a2eb7
GunkanjimaQA <47102954+GunkanjimaQA@users.noreply.github.com>
2019-12-05 21:41:16 +0200

    Added connection to Database Added first database test

--- kuzmichJanchukProj/src/main/java/pages/pageelements/LeftMenu.java
+++ kuzmichJanchukProj/src/main/java/pages/pageelements/LeftMenu.java
@@ -1,11 +1,10 @@
 package pages.pageelements;
 
 import org.openqa.selenium.support.FindBy;
-import ru.yandex.qatools.htmlelements.element.HtmlElement;
 import ru.yandex.qatools.htmlelements.element.Link;
 
 @FindBy(xpath = ".//*[@class='sidebar-menu']")
-public class LeftMenu extends HtmlElement {
+public class LeftMenu extends CommonActionsWithElements {
 
     @FindBy(id = "dictionary")
     private Link menuDictionary;
