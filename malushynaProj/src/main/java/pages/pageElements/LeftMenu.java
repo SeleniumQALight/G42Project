0851ba4cfdd1fcf07036e4d09a10def3dfd54e20
EvheniiaM <jonsonseee@gmail.com>
2019-12-05 20:02:11 +0200

    HTMLElements

--- malushynaProj/src/main/java/pages/pageElements/LeftMenu.java
+++ malushynaProj/src/main/java/pages/pageElements/LeftMenu.java
@@ -6,7 +6,7 @@ import ru.yandex.qatools.htmlelements.element.Link;
 
 
 @FindBy(xpath = ".//*[@class='sidebar-menu']")
-public class LeftMenu extends HtmlElement {
+public class LeftMenu extends CommonActionWithElements {
 
     @FindBy(id = "dictionary")
     private Link menuDictionary;
@@ -15,10 +15,10 @@ public class LeftMenu extends HtmlElement {
     private Link subMenuSpares;
 
     public void clickOnMenuDictionary(){
-        menuDictionary.click();
+        actionsWithOurElements.clickOnElement(menuDictionary);
     }
 
     public void clickOnSubMenuSpare() {
-        subMenuSpares.click();
+        actionsWithOurElements.clickOnElement(subMenuSpares);
     }
 }
